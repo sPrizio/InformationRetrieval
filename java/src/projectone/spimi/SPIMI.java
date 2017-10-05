a8565f557618556e446bbb00b3d50f29bb5e493b
Stephen <s.prizio@hotmail.com>
2017-10-05 14:22:23 -0400

    adds shell for spimi algorithm

---
+++ java/src/projectone/spimi/SPIMI.java
@@ -0,0 +1,23 @@
+package projectone.spimi;
+
+import projectone.entity.Dictionary;
+
+import java.io.File;
+
+/**
+ * Collection of Single-pass in-memory indexing algorithms for use in an information retrieval system
+ *
+ * @author Stephen Prizio
+ */
+public class SPIMI {
+    private Dictionary dictionary;
+
+    public SPIMI() {
+        this.dictionary = new Dictionary();
+    }
+
+    public File spimiInvert() {
+        //  refer to https://nlp.stanford.edu/IR-book/html/htmledition/single-pass-in-memory-indexing-1.html
+        return new File("TEMP");
+    }
+}
